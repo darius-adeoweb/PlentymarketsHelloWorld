cc03bb6b31f7dc7291542c328c09a86bf989cbc0
darius-adeoweb <darius@adeoweb.lt>
2019-02-27 13:24:34 +0200

    update 2

--- src/Providers/PlentymarketsHelloWorldRouteServiceProvider.php
+++ src/Providers/PlentymarketsHelloWorldRouteServiceProvider.php
@@ -4,12 +4,11 @@ namespace PlentymarketsHelloWorld\Providers;
 
 use Plenty\Plugin\RouteServiceProvider;
 use Plenty\Plugin\Routing\Router;
-use PlentymarketsHelloWorld\Controllers\ContentController;
 
 class PlentymarketsHelloWorldRouteServiceProvider extends RouteServiceProvider
 {
     public function map(Router $router)
     {
-        $router->get('hello', ContentController::class . '@sayHello');
+        $router->get('hello', 'PlentymarketsHelloWorld\Controllers\ContentController@sayHello');
     }
 }
